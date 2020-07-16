803dd200e2a5f8d5e261be0d664b83cb4d04d3cf
TirumaliSaiTeja <tsaiteja7@gmail.com>
2020-07-16 22:41:54 +0530

    initial commit

---
+++ migrations/2_deploy_contract.js
@@ -0,0 +1,14 @@
+const Token = artifacts.require('Token')
+const EthSwap = artifacts.require('EthSwap')
+
+module.exports = async function (deployer) {
+  // Deploying token
+  await deployer.deploy(Token)
+  const token = await Token.deployed()
+
+  // Deploying EthSwap
+  await deployer.deploy(EthSwap)
+  const ethSwap = await EthSwap.deployed()
+
+  await token.transfer(ethSwap.address, '1000000000000000000000000')
+}
