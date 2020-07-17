4253b4ea84e67022be317c009d2f35422669513b
TirumaliSaiTeja <tsaiteja7@gmail.com>
2020-07-17 22:57:22 +0530

    testing contract

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,14 +1,14 @@
-const Token = artifacts.require('Token')
-const EthSwap = artifacts.require('EthSwap')
+const Token = artifacts.require("Token");
+const EthSwap = artifacts.require("EthSwap");
 
-module.exports = async function (deployer) {
+module.exports = async function(deployer) {
   // Deploying token
-  await deployer.deploy(Token)
-  const token = await Token.deployed()
+  await deployer.deploy(Token);
+  const token = await Token.deployed();
 
   // Deploying EthSwap
-  await deployer.deploy(EthSwap)
-  const ethSwap = await EthSwap.deployed()
+  await deployer.deploy(EthSwap, token.address);
+  const ethSwap = await EthSwap.deployed();
 
-  await token.transfer(ethSwap.address, '1000000000000000000000000')
-}
+  await token.transfer(ethSwap.address, "1000000000000000000000000");
+};
--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -1,5 +1,22 @@
 pragma solidity ^0.5.0;
 
+import "./Token.sol";
+
 contract EthSwap {
     string public name = "Eth Swap";
+    // name here is a state variable
+    Token public token;
+    uint256 public rate = 100;
+
+    constructor(Token _token) public {
+        token = _token;
+    }
+
+    function buyTokens() public payable {
+        // Redemption rate = # of tokens they receive for 1 ether
+
+        // Amount of ethereum * redemption rate
+        uint256 tokenAmount = msg.value * rate;
+        token.transfer(msg.sender, tokenAmount);
+    }
 }
